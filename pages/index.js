a65c91af7048e7f53c27927f7f1d221f53329eaf
AslanSN <aslan.staub@manumartinez.es>
2022-09-05 18:35:40 +0200

    index ' fix

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ allPostsData }) {
 					<a href='https://www.linkedin.com/in/alanstaubnegro/'>Linkedin</a>.
 				</p>
 				<p>
-					(This is a sample website - you’ll be building a site like this on{' '}
+					(This is a sample website - you will be building a site like this on{' '}
 					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
 				</p>
 			</section>
